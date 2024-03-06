6548cd28eabf830ab8ab5a047c2b0cef04a26057
optic4l <lksdro01@gmail.com>
2024-03-06 09:35:33 -0300

    ajuste modelo avance

--- avance/models.py
+++ avance/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from obras.models import Obras
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 
 class Avances(models.Model):
     id_obra = models.ForeignKey(Obras, on_delete=models.CASCADE)
     fecha = models.DateField()
-    porcentaje = models.IntegerField()
+    porcentaje = models.IntegerField(validators=[MaxValueValidator(100), MinValueValidator(0)])
     tipo = models.CharField(max_length=255, null=False)

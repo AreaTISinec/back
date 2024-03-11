95debcc52999d0f1dc1bfca40e04072529a33fc4
optic4l <lksdro01@gmail.com>
2024-03-11 13:37:51 -0300

    se añadio campos necesarios

--- historial_financiero/models.py
+++ historial_financiero/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from perfil_usuario.models import UserProfile
 from obras.models import Obras
+from empresas.models import Empresa
 
 # Create your models here.
 class Historial(models.Model):
@@ -10,3 +11,5 @@ class Historial(models.Model):
     responsable = models.ForeignKey(UserProfile, on_delete=models.DO_NOTHING)
     porcentaje = models.IntegerField(default=0)
     id_obra = models.ForeignKey(Obras, on_delete=models.CASCADE)
+    numero_factura = models.IntegerField(default=0)
+    empresa = models.ForeignKey(Empresa, on_delete=models.DO_NOTHING, default=1)

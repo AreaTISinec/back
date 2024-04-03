e1c0de96cc106ed998696ed4a21ecd2f90253e5c
optic4l <lksdro01@gmail.com>
2024-04-03 11:00:52 -0300

    csu

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path, re_path
+from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import TokenRefreshView
+
 from accounts.views import CustomTokenObtainPairView
 
 urlpatterns = [
@@ -28,3 +29,4 @@ urlpatterns = [
 static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += [re_path(r'^.*', TemplateView.as_view(template_name='index.html'))]
+
